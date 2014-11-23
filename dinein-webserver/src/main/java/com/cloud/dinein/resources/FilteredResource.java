3c71cc7e21f861fd0fbddf0816ee342bf0922b67
Sai Tejaswini <tejaswini.mannava@nyu.edu>
2014-11-23 16:13:29 -0500

    Refactoring into two directories

---
+++ dinein-webserver/src/main/java/com/cloud/dinein/resources/FilteredResource.java
@@ -0,0 +1,17 @@
+package com.cloud.dinein.resources;
+
+import com.cloud.dinein.filter.DateRequired;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/filtered")
+public class FilteredResource {
+
+    @GET
+    @DateRequired
+    @Path("hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
